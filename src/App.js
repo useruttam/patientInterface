2f7ae0fa78bd71edb257e0bd5d584bba9c81d7c7
Uttam <uttam.kumar.97091@gmail.com>
2022-03-10 16:19:43 +0530

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, Fragment } from "react";
 import './App.css';
+import pic from "./images/loginph.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+export default function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app container p-5">
+    
+    <br/>
+    <center>
+    <img src={pic}/>
+    <br/>
+    <center>
+    <button id="fks" type="button" class="btn btn-secondary" onClick="#" >Username</button>
+    <br/>
+    <button id="fks" type="button" class="btn btn-secondary" onClick="#" >PID</button>
+   </center>
+    </center>
+    <table>
+    <tr>
+    <td><button type="button" class="btn btn-primary">View Health Records</button> </td>
+    <td><button type="button" class="btn btn-primary" style={{float:"right"}}>View Personal Info</button> </td>
+    </tr>
+    </table>
+    
+       </div>
   );
 }
-
-export default App;
